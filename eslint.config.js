e65dddf08874163e5f6f8ff3b4e76abb4a273b7d
Edward Westbury <eddie.westbury@gmail.com>
2025-01-10 14:25:51 -0500

    Add ESLint plugins for TypeScript and boundaries configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,49 +1,226 @@
 // @ts-check
-const eslint = require("@eslint/js");
-const tseslint = require("typescript-eslint");
-const angular = require("angular-eslint");
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const boundaries = require('eslint-plugin-boundaries');
 
 module.exports = tseslint.config(
   {
-    files: ["**/*.ts"],
+    files: ['**/*.ts'],
+    plugins: { boundaries },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.recommended,
       ...tseslint.configs.stylistic,
       ...angular.configs.tsRecommended,
+      boundaries.configs.strict,
     ],
     processor: angular.processInlineTemplates,
     rules: {
-      "@angular-eslint/directive-selector": [
-        "error",
+      "boundaries/no-unknown-files": [0],
+      'boundaries/element-types': [
+        'error',
         {
-          type: "attribute",
-          prefix: "app",
-          style: "camelCase",
+          default: 'disallow',
+          rules: [
+            {
+              from: 'main',
+              allow: [['app', { app: '${from.app}' }]],
+            },
+            {
+              from: 'core',
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['core', { app: '${from.app}' }],
+              ],
+            },
+            {
+              from: 'ui',
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['ui', { app: '${from.app}' }],
+              ],
+            },
+            {
+              from: 'layout',
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['core', { app: '${from.app}' }],
+                ['ui', { app: '${from.app}' }],
+                ['pattern', { app: '${from.app}' }],
+              ],
+            },
+            {
+              from: 'app',
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['app', { app: '${from.app}' }],
+                ['core', { app: '${from.app}' }],
+                ['layout', { app: '${from.app}' }],
+                ['feature-routes', { app: '${from.app}' }],
+              ],
+            },
+            {
+              from: ['pattern'],
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['core', { app: '${from.app}' }],
+                ['ui', { app: '${from.app}' }],
+                ['pattern', { app: '${from.app}' }],
+              ],
+            },
+            {
+              from: ['feature'],
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['core', { app: '${from.app}' }],
+                ['ui', { app: '${from.app}' }],
+                ['pattern', { app: '${from.app}' }],
+              ],
+            },
+            {
+              from: ['feature-routes'],
+              allow: [
+                ['lib-api'],
+                ['env', { app: '${from.app}' }],
+                ['core', { app: '${from.app}' }],
+                ['pattern', { app: '${from.app}' }],
+                ['feature', { app: '${from.app}', feature: '${from.feature}' }],
+                [
+                  'feature-routes',
+                  { app: '${from.app}', feature: '!${from.feature}' },
+                ],
+              ],
+            },
+            {
+              from: ['lib-api'],
+              allow: [['lib', { app: '${from.lib}' }]],
+            },
+            {
+              from: ['lib'],
+              allow: [['lib', { app: '${from.lib}' }]],
+            },
+          ],
         },
       ],
-      "@angular-eslint/component-selector": [
-        "error",
+      '@angular-eslint/directive-selector': [
+        'error',
         {
-          type: "element",
-          prefix: "app",
-          style: "kebab-case",
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
         },
       ],
-      "@typescript-eslint/consistent-type-assertions": [
-        "error",
+      '@angular-eslint/component-selector': [
+        'error',
         {
-          assertionStyle: "angle-bracket",
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
         },
-      ]
+      ],
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        {
+          assertionStyle: 'angle-bracket',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+      },
+      'boundaries/ignore': [],
+      'boundaries/dependency-nodes': ['import', 'dynamic-import'],
+      'boundaries/elements': [
+        {
+          type: 'env',
+          pattern: 'environments',
+          basePattern: 'projects/**/src',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'main',
+          mode: 'file',
+          pattern: 'main.ts',
+          basePattern: 'projects/**/src',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'app',
+          mode: 'file',
+          pattern: 'app(-|.)*.ts',
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'core',
+          pattern: 'core',
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'ui',
+          pattern: 'ui',
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'layout',
+          pattern: 'layout',
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'pattern',
+          pattern: 'pattern',
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'feature-routes',
+          mode: 'file',
+          pattern: 'feature/*/*.routes.ts',
+          capture: ['feature'],
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+        {
+          type: 'feature',
+          pattern: 'feature/*',
+          capture: ['feature'],
+          basePattern: 'projects/**/src/app',
+          baseCapture: ['app'],
+        },
+
+        {
+          type: 'lib-api',
+          mode: 'file',
+          pattern: 'projects/**/src/public-api.ts',
+          capture: ['lib'],
+        },
+        {
+          type: 'lib',
+          pattern: 'projects/**/src/lib',
+          capture: ['lib'],
+        },
+      ],
     },
   },
   {
-    files: ["**/*.html"],
+    files: ['**/*.html'],
     extends: [
       ...angular.configs.templateRecommended,
       ...angular.configs.templateAccessibility,
     ],
     rules: {},
-  }
+  },
 );
