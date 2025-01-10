be50b4787737d2b901d7ebf1b0ec7c491ba7ff96
Edward Westbury <eddie.westbury@gmail.com>
2025-01-10 14:01:54 -0500

    Implement main layout and home feature with routing

--- projects/blueprint/src/app/app.routes.ts
+++ projects/blueprint/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./feature/home/home.routes')
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ projects/blueprint/src/app/feature/home/home.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from "@angular/router";
+
+export default <Routes>[
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'home'
+  // },
+  {
+    path: '',
+    loadComponent: () => import("./home/home.component").then(m => m.HomeComponent)
+  }
+];
